1435f057ae24231942af5e58b352a8d54a317a18
Julio Berina <julioberina@gmail.com>
2018-08-15 16:28:31 -0700

    Change questions

--- client/questionnaire/src/Answers.js
+++ client/questionnaire/src/Answers.js
@@ -37,49 +37,11 @@ class Answers extends Component {
         <Form>
           <Header as='h1'>Questionnaire</Header><br />
 
-          <Question text='Question 1. What is your age group?'>
-            <Radio label='10-14 (Middle School)' />
-            <Radio label='14-18 (High School)' />
-            <Radio label='18-65 (Adult)' />
+          <Question text='When did you complete training?'>
+            <input type="date" name="training" />
           </Question>
 
-          <Question text='Question 2. Do you have any children?'>
-            <Radio label='Yes' />
-            <Radio label='No' />
-          </Question>
-
-          <Question text='Question 3. Are you currently working?'>
-            <Radio label='Yes' />
-            <Radio label='No' />
-          </Question>
-
-          <Question text='Question 4. Are you facing housing hardships?'>
-            <Radio label='Yes' />
-            <Radio label='No' />
-          </Question>
-
-          <Question text='Question 5. Are you facing financial hardships?'>
-            <Radio label='Yes' />
-            <Radio label='No' />
-          </Question>
-
-          <Question text='Question 6. Do you or any of the adults in your immediate family have a disability?'>
-            <Radio label='Yes' />
-            <Radio label='No' />
-            <Radio label='Prefer not to disclose' />
-          </Question>
-
-          <Question text='Question 7. Are you or someone in your immediate family a senior citizen?'>
-            <Radio label='Yes' />
-            <Radio label='No' />
-          </Question>
-
-          <Question text='Question 8. Are you or someone in your immediate family a teacher?'>
-            <Radio label='Yes' />
-            <Radio label='No' />
-          </Question>
-
-          <Question text='Question 9. Would you like to include organizations that offer services to the LGBTQIA community?'>
+          <Question text='Did you find a job?'>
             <Radio label='Yes' />
             <Radio label='No' />
           </Question>
