76a7c1e9212d39209ff910249a04afbc520e86a3
Julio Berina <julioberina@gmail.com>
2018-08-16 16:37:40 -0700

    Fix toggle issue with radio buttons and set up global state

--- client/questionnaire/src/Answers.js
+++ client/questionnaire/src/Answers.js
@@ -1,24 +1,40 @@
 import React, { Component } from 'react';
 import { Container, Header, Form, Radio, Button } from 'semantic-ui-react';
 
+let answers = {
+  
+};
+
 class Question extends Component {
+  render() {
+    return (
+      <Form.Field>
+        <Header as='h4'>{this.props.text}</Header>
+      </Form.Field>
+    );
+  }
+}
+
+class JobFoundRadioGroup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleChange = (e, { value }) => this.setState({ value });
+  }
+
   render() {
     return (
       <div>
         <Form.Field>
-          <Header as='h4'>{this.props.text}</Header>
+          <Radio name="jobgroup" label='Yes'
+            value='yes' checked={this.state.value === 'yes'}
+            onChange={this.handleChange} />
         </Form.Field>
-
-        {
-          React.Children.map(this.props.children, (child, i) => {
-            return (
-              <Form.Field>
-                {child}
-              </Form.Field>
-            );
-          })
-        }
-
+        <Form.Field>
+          <Radio name="jobgroup" label='No'
+            value='no' checked={this.state.value === 'no'}
+            onChange={this.handleChange} />
+        </Form.Field>
         <br />
       </div>
     );
@@ -26,25 +42,19 @@ class Question extends Component {
 }
 
 class Answers extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
   render() {
     return(
       <Container text className='questionnaire-style'>
         <Form>
           <Header as='h1'>Questionnaire</Header><br />
 
-          <Question text='When did you complete training?'>
+          <Question text='When did you complete training?' />
+          <Form.Field>
             <input type="date" name="training" />
-          </Question>
+          </Form.Field><br />
 
-          <Question text='Did you find a job?'>
-            <Radio name="jobgroup" label='Yes' />
-            <Radio name="jobgroup" label='No' />
-          </Question>
+          <Question text='Did you find a job?' />
+          <JobFoundRadioGroup />
 
           <Button primary type='submit'>Submit</Button><br /><br /><br />
         </Form>
