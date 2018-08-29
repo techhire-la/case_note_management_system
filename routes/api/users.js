73d77a431c45f4c84aa5abf67efe9412ff972e43
Jupiter <jupiterbaudot@gmail.com>
2018-08-29 11:59:35 -0700

    Discard broken code

--- routes/api/users.js
+++ routes/api/users.js
@@ -121,7 +121,7 @@ router.post('/register', (req, res) => {
   if (!isValid) {
     return res.status(400).json(errors);
   }
-\
+
   User.findOne({ email: req.body.email }).then(user => {
 
     if (user) {
