4c5f804d3824a18df90f42e3b5676590aea14923
Jupiter <jupiterbaudot@gmail.com>
2018-09-18 11:53:15 -0700

    Add redux for getting dashboard (still in works)

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import Login from './components/Login'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Dashboard from './components/dashboard/Dashboard'
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
+
+import PrivateRoute from './components/common/PrivateRoute';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -46,6 +49,9 @@ class App extends Component {
                     <div className="container">
                         <Route exact path="/" component={Login} />
                     </div>
+                    <Switch>
+                        <PrivateRoute exact path="/dashboard" component={Dashboard} />
+                    </Switch>
                 </Router>
             </div>
         </Provider>
--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,8 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+// import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+
 
 const initialState = {};
 
